82527ac7ae5dde3722ebe679cdd6bed3d80f3c95
AlapinEnjoyer <120122957+AlapinEnjoyer@users.noreply.github.com>
2026-09-07 22:12:26 +0200

    feat: Add macos pathbar and FoldersFirst sort

--- modules/darwin/defaults.nix
+++ modules/darwin/defaults.nix
@@ -2,6 +2,11 @@
   system.defaults = {
     NSGlobalDomain.AppleShowAllExtensions = true;
 
+    finder = {
+      _FXSortFoldersFirst = true;
+      ShowPathbar = true;
+    };
+
     dock = {
       autohide = true;
       "autohide-delay" = 0.0;
